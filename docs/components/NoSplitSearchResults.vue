8ddc8a05d151bb6fa6ef36a82dec43de608fae1d
Andy Ghiuta <aghiuta@ena.com>
2021-10-12 15:42:00 +0300

    Add new option for splitting search string
    When passed as false it will use the full text, along with spaces

---
+++ docs/components/NoSplitSearchResults.vue
@@ -0,0 +1,19 @@
+<template>
+  <treeselect
+    :options="options"
+    :multiple="true"
+    :search-split-words="false"
+    :flatten-search-results="false"
+    placeholder="Where are you from?"
+    />
+</template>
+
+<script>
+  import countries from './data/countries-of-the-world'
+
+  export default {
+    data: () => ({
+      options: countries,
+    }),
+  }
+</script>
